338b5e3825847eedfd693fe202550b1a71a0dfc0
Duojay Williams <dhwilliams3007@gmail.com>
2023-11-11 11:11:41 -0800

    added search bar component to nav and centered it

--- image-search/frontend/src/Nav.jsx
+++ image-search/frontend/src/Nav.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import h from './Home'
+import User from './images/use.png';
 import './nav.css'
+import SearchBar from "./SearchImages";
 function Nav() {
   return (
     <nav>
-  
-    <a href="/">Home</a>
-    <a href="./search">Search Images</a>
-  <a href="./shares.html">My Shares</a>
+  <ul>
+    <a href="/" className='home'>Home</a>
+    {/* <SearchBar/> */}
+    <a href="./search"> <SearchBar/></a>
+    <img src={User} className='icon'/>
+  {/* <a href="./shares.html">My Shares</a>
     <a href="./user.html">Profile</a>
-    <a href="./likes.html">Likes</a>
+    <a href="./likes.html">Likes</a> */}
+    </ul>
     </nav>
   )
 }
