fb73b112c0b050c9672294968e9021ca4c1d5443
Duojay Williams <dhwilliams3007@gmail.com>
2023-11-30 18:58:46 -0800

    Committing unstaged changes before rebase

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@
 // // // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
+// import { defineConfig } from "vite";
+
 // // // https://vitejs.dev/config/
 // // export default defineConfig({
 // //   plugins: [react()],
@@ -31,15 +33,34 @@
 //   },
 // };
 
-export default {
-  build: {
-    chunkSizeWarningLimit: 1000, // set your desired size limit in kilobytes
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
+// export default {
+//   build: {
+//     chunkSizeWarningLimit: 1000, // set your desired size limit in kilobytes
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom'],
+//         },
+//       },
+//     },
+//   },
+// };
+// import {defineConfig} from "vite"
+// import vue from "@vitejs/plugin-react"
+// export default defineConfig({
+//   base: "/test-deploy/",
+//   plugins:[()]
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
   },
-};
+});
